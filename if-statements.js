3f6502aa7c0702a1e4a08a196d907cf1f280680a
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-22 17:00:26 +0530

    Ex9: Verified

---
+++ if-statements.js
@@ -0,0 +1,7 @@
+var fruit = 'orange';
+
+if(fruit.length <= 5)
+  console.log('The fruit name has ive characters or less.');
+else {
+  console.log('The fruit name has more than five characters.');
+}
