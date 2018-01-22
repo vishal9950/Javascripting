fc47ad8592adb94cd479e268cba0af404ca0236f
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-01-22 17:02:16 +0530

    Ex15: Verified

---
+++ objects.js
@@ -0,0 +1,16 @@
+/*var pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2
+};
+
+console.log(pizza);*/
+console.log(banana);
+console.log(eat(banana = 'banana'));
+
+function eat(food) {
+  return food + ' df';
+}
+
+//console.log(banana)
+//banana = 'banana
